689b32fb3b50b4ab823da1ad0bc429d4c9532f5f
raj-vora <raj.jv@somaiya.edu>
2020-10-30 17:04:42 +0530

    add drawer to homes_creen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_social_ui/data/data.dart';
+import 'package:flutter_social_ui/widgets/custom_drawer.dart';
 import 'package:flutter_social_ui/widgets/following_users.dart';
 import 'package:flutter_social_ui/widgets/posts_carousel.dart';
 
@@ -54,6 +55,7 @@ class _HomeScreenState extends State<HomeScreen>
           ],
         ),
       ),
+      drawer: CustomDrawer(),
       body: ListView(
         children: <Widget>[
           FollowingUsers(),
---
+++ lib/widgets/custom_drawer.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_social_ui/data/data.dart';
+import 'package:flutter_social_ui/screens/home_screen.dart';
+import 'package:flutter_social_ui/screens/login_screen.dart';
+
+class CustomDrawer extends StatelessWidget {
+  _buildDrawerOption(Icon icon, String title, Function onTap) {
+    return ListTile(
+      leading: icon,
+      title: Text(title,
+          style: TextStyle(
+            fontSize: 20.0,
+          )),
+      onTap: onTap,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Image(
+                height: 200.0,
+                width: double.infinity,
+                image: AssetImage(currentUser.backgroundImageUrl),
+                fit: BoxFit.cover,
+              ),
+              Positioned(
+                bottom: 20.0,
+                left: 20.0,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: [],
+                        border: Border.all(
+                          width: 3.0,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      child: ClipOval(
+                        child: Image(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            currentUser.profileImageUrl,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 6.0),
+                    Text(
+                      currentUser.name,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          _buildDrawerOption(
+            Icon(Icons.dashboard),
+            'Home',
+            () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => HomeScreen(),
+              ),
+            ),
+          ),
+          _buildDrawerOption(Icon(Icons.account_circle), 'My Profile', () {}),
+          _buildDrawerOption(Icon(Icons.chat), 'Chat', () {}),
+          _buildDrawerOption(Icon(Icons.location_on), 'Map', () {}),
+          _buildDrawerOption(Icon(Icons.settings), 'Settings', () {}),
+          Expanded(
+            child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: _buildDrawerOption(
+                Icon(Icons.exit_to_app),
+                'Logout',
+                () => Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginScreen(),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
