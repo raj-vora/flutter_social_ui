d5b909c63431f0a3454ac285f5885e06f4cc9d8c
raj-vora <raj.jv@somaiya.edu>
2020-10-30 11:23:04 +0530

    following users horizontal stories

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_social_ui/data/data.dart';
+import 'package:flutter_social_ui/widgets/following_users.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+        title: Text(
+          'FRENZY',
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 10.0,
+          ),
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          labelColor: Theme.of(context).primaryColor,
+          labelStyle: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w600,
+          ),
+          unselectedLabelStyle: TextStyle(
+            fontSize: 18.0,
+          ),
+          indicatorWeight: 3.0,
+          tabs: <Widget>[
+            Text('Trending'),
+            Text('Latest'),
+          ],
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          FollowingUsers(),
+        ],
+      ),
+    );
+  }
+}
